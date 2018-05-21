2bdf7eafe91378c47c8f4d86daccc189c9d1867e
Matias Delgado <matiasdel@gmail.com>
2018-05-21 19:37:53 -0300

    Replace chevron image with png

--- content.js
+++ content.js
@@ -1,12 +1,10 @@
 if (!initialized()) {
   let files = Array.from(document.querySelectorAll('.js-file-content'));
   const totalFiles = +document.querySelector('#files_tab_counter').textContent;
+
   console.info(`Adding collapse buttons: ${files.length}/${totalFiles}`);
 
-  files.forEach((file, index) => {
-    const button = createButton(index + 1);
-    file.appendChild(button);
-  });
+  files.forEach(appendButton);
 
   if (files.length < totalFiles) {
     observe(files.length, totalFiles);
@@ -23,7 +21,7 @@ function observe(startCount, totalFiles) {
       mutation.addedNodes.forEach(file => {
         let fileContent;
         if (file.querySelector && (fileContent = file.querySelector(selector))) {
-          fileContent.appendChild(createButton(++count));
+          appendButton(fileContent, count++);
           if (count == totalFiles) {
             observer.disconnect();
             console.info('Observer disconnected');
@@ -48,19 +46,14 @@ function collapseFile(target) {
   file.scrollIntoView();
 }
 
-function createButton(index) {
-  const button = document.createElement('span');
-  button.innerHTML = svgContent();
-  button.classList = ['collapse-icon'];
-  button.title = 'Collapse file ' + index;
-  button.style = 'position: absolute; bottom: 0; right: 10px; cursor: pointer;';
-  button.addEventListener('click', collapseFile);
-  return button;
-}
-
-// TODO: convert to png
-function svgContent() {
-  return `<?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg height="16px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="10px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M256,213.7L256,213.7L256,213.7l174.2,167.2c4.3,4.2,11.4,4.1,15.8-0.2l30.6-29.9c4.4-4.3,4.5-11.3,0.2-15.5L264.1,131.1  c-2.2-2.2-5.2-3.2-8.1-3c-3-0.1-5.9,0.9-8.1,3L35.2,335.3c-4.3,4.2-4.2,11.2,0.2,15.5L66,380.7c4.4,4.3,11.5,4.4,15.8,0.2L256,213.7  z"/></svg>`;
+function appendButton(file, index) {
+  const img = document.createElement('img');
+  img.src = chrome.runtime.getURL('if_icon-chevron-up_211648.png');
+  img.classList = ['collapse-icon'];
+  img.title = 'Collapse file ' + (index + 1);
+  img.style = 'position: absolute; bottom: 6px; right: 10px; width: 10px; cursor: pointer;';
+  img.addEventListener('click', collapseFile);
+  file.appendChild(img);
 }
 
 function initialized() {
